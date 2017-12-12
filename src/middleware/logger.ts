5226d856cebcd6bbb115a9615cd296ec5bd65baa
Dylan Pyle <me@dylanpyle.com>
2017-12-12 12:45:31 -0800

    add colored logging

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,37 +1,20 @@
-import * as qs from 'querystring';
 import * as Koa from 'koa';
 
+import * as COLORS from '../services/colors';
+
 // Middleware to log timing & info for each incoming request
-
-interface Data { [key: string]: string | number; }
-
-function stringify(obj: Data): string {
-  const escaped: Data = {};
-
-  Object.keys(obj).forEach((key: string): void => {
-    escaped[key] = JSON.stringify(obj[key]);
-  });
-
-  return qs.stringify(escaped, ', ', '=', {
-    encodeURIComponent: (val: string): string => val
-  });
-}
 
 async function logger(ctx: Koa.Context, next: () => void): Promise<any> {
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
 
-  /* tslint:disable-next-line:no-console */
-  console.log(stringify({
-    connectingIp: ctx.request.headers['cf-connecting-ip'],
-    method: ctx.method,
-    requestIp: ctx.request.ip,
-    responseTime: ms,
-    status: ctx.status,
-    url: ctx.url,
-    userAgent: ctx.request.headers['user-agent']
-  }));
+  const ip = ctx.request.headers['cf-connecting-ip'] || ctx.request.ip;
+  const ua = ctx.request.headers['user-agent'];
+
+  const statusColor = (ctx.status < 400) ? COLORS.green : COLORS.red;
+  // tslint:disable-next-line
+  console.log(`${statusColor}${ctx.status}${COLORS.reset} ${ctx.method} ${COLORS.blue}${ctx.url}${COLORS.reset} ms:${ms} ip:${ip} ua:"${ua}"`);
 }
 
 export default logger;
